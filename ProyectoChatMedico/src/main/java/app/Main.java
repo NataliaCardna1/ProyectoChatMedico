ebf546b225c397f951b9507f8a93182351a050ee
andresLamsu <perezmartinezandres0@gmail.com>
2024-11-19 09:20:44 -0500

    Ventanas login y chat

--- ProyectoChatMedico/src/main/java/app/Main.java
+++ ProyectoChatMedico/src/main/java/app/Main.java
@@ -1,9 +1,22 @@
 package app;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import modelo.*;
 
+import static javafx.application.Application.launch;
 
-public class Main {
+
+public class Main extends Application {
     public static void main(String[] args) {
+
+
+
+        launch(Main.class, args);
+
+/*
         var usuario = new Usuario("U1", "Julian");
         var usuario2 = new Usuario("U2", "Natalia");
         var medico = new Usuario("U1", "Julian");
@@ -25,9 +38,27 @@ public class Main {
 
 //        for (Documento documentoUsuario : usuario2.getDocumentos() ) {
 //            System.out.println(documentoUsuario.nombre());
-//        }
+//        }*/
 
 
+    }
+
+
+
+    public void start(Stage primaryStage) {
+        try {
+            // Carga el archivo FXML desde recursos
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("org/example/proyectochatmedico/Login/Login.fxml"));
+            Parent parent = loader.load();
+
+            Scene scene = new Scene(parent);
+
+            primaryStage.setTitle("Mi Ventana");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
